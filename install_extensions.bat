2f2b5e480c9984c2ccd1f29757bea390276aa3e8
Jake Clarey <jakeclarey@gmail.com>
2024-11-07 08:22:59 -0500

    updated install extensions batchfile

--- install_extensions.bat
+++ install_extensions.bat
@@ -1,8 +1,6 @@
 @echo off
 REM Install Visual Studio Code extensions
-code --install-extension ms-vscode.cpptools
-code --install-extension marus25.cortex-debug
-code --install-extension ms-vscode.makefile-tools
+code --install-extension ms-vscode.cpptools --install-extension marus25.cortex-debug --install-extension ms-vscode.makefile-tools
 echo Visual Studio Code extensions installed!
 echo Batchfile run completed!
 pause
